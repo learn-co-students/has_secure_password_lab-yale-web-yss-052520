614829308f5aee9c6f85e72b16cfbf466605819e
eric <eric.ou@yale.edu>
2020-06-27 17:26:13 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
+
+    def new()
+    end
+    
     def create
         @user = User.find_by(name: params[:user][:name])
-        return head(:forbidden) unless @user.authenticate(params[:user][:password])
+        authenticated = @user.try(:authenticate, params[:user][:password])
+        return redirect_to(controller: 'sessions', action: 'new') unless authenticated
         session[:user_id] = @user.id
+        redirect_to welcome_path(@user)
+    end
+
+    def destroy()
+      session.delete(:user_id)
+      redirect_to login_path
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
     def new()
+        @user = User.new()
     end
 
     def create()
         @user = User.new(user_params)
-        redirect_to(new_user_path) unless @user.save
+        return redirect_to(new_user_path) unless @user.save
         session[:user_id] = @user.id
+        redirect_to welcome_path(@user)
+    end
+
+    def welcome()
+        return head(:forbidden) unless session.include? :user_id
+        @user = User.find(session[:user_id])
     end
 
     private
