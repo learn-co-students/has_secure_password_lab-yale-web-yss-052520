88f419eb3901e57ed59a7c72692e08cb013de367
ev275 <enrique.valencia@yale.edu>
2020-07-04 18:23:10 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.find_by(name: params[:user][:name])
+        user = user.try(:authenticate, params[:user][:password])
+        # byebug
+        if user
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+
+        redirect_to root_path
+    end
+end
